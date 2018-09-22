4cf8dfb6cb0d3105ecc19aa30634b235c8c32fa1
cbowen <cbowen@cse.msu.edu>
2018-09-22 19:47:22 -0400

    Includes Javascript build

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,21 @@
 const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
 	entry: {
 		Users: path.resolve(__dirname, 'index.js'),
 		UsersConsole: path.resolve(__dirname, 'js/Console/index.js'),
 		Login: path.resolve(__dirname, 'js/Login/Login.js')
-	}
+	},
+	plugins: [
+		new FileManagerPlugin({
+			onEnd: {
+				copy: [
+					{source: path.resolve(__dirname, '../../../cl/dist/users.*'), destination: path.resolve(__dirname, 'dist') },
+					{source: path.resolve(__dirname, '../../../cl/dist/usersconsole.*'), destination: path.resolve(__dirname, 'dist') },
+					{source: path.resolve(__dirname, '../../../cl/dist/login.*'), destination: path.resolve(__dirname, 'dist') }
+				]
+			}
+		})
+	]
 }
