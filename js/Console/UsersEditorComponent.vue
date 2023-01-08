11a5ce2e7f8a42468856380e9a707dc185775b3f
Dr. Charles B. Owen <cbowen@msu.edu>
2023-01-07 21:38:54 -0500

    Fixed but when using UsersEditorComponent not as a management page

--- js/Console/UsersEditorComponent.vue
+++ js/Console/UsersEditorComponent.vue
@@ -39,40 +39,40 @@
 </template>
 
 <script>
-    import Dialog from 'dialog-cl';
-    import FetcherVue from '../Lib/FetcherVue.vue';
+import Dialog from 'dialog-cl';
+import FetcherVue from '../Lib/FetcherVue.vue';
 
-    const mapState = Site.Vuex.mapState;
-    const ConsoleComponentBase = Site.ConsoleComponentBase;
+const mapState = Site.Vuex.mapState;
+const ConsoleComponentBase = Site.ConsoleComponentBase;
 
-    export default {
-      'extends': ConsoleComponentBase,
-      props: [
-            'management'
-        ],
-        data: function() {
-            return {
-            	root: Site.root,
-                pencil: Site.root + '/vendor/cl/site/img/pencil16.png',
-                deleter: Site.root + '/vendor/cl/site/img/x.png',
-                info: Site.root + '/vendor/cl/site/img/info16.png',
-                toUser: function(user) {
-                      return Site.root + '/cl/console/management/user/' + user.id;
-                }
-            }
-        },
-        methods: {
-            fetchMore() {
-                this.$store.dispatch('users/more');
-            },
-            deleteUser: function(user) {
-                new Dialog.MessageBox('Are you sure?', 'Are you sure you want to delete ' + user.name,
-                    Dialog.MessageBox.OKCANCEL, () => {
-                      this.$store.dispatch('users/delete', {id: user.id});
-                    });
-            },
-            presentUser: function(user) {
-                let content = `<div class="cl-dialog">
+export default {
+  'extends': ConsoleComponentBase,
+  props: [
+    'management'
+  ],
+  data: function () {
+    return {
+      root: Site.root,
+      pencil: Site.root + '/vendor/cl/site/img/pencil16.png',
+      deleter: Site.root + '/vendor/cl/site/img/x.png',
+      info: Site.root + '/vendor/cl/site/img/info16.png',
+      toUser: function (user) {
+        return Site.root + '/cl/console/management/user/' + user.id;
+      }
+    }
+  },
+  methods: {
+    fetchMore() {
+      this.$store.dispatch('users/more');
+    },
+    deleteUser: function (user) {
+      new Dialog.MessageBox('Are you sure?', 'Are you sure you want to delete ' + user.name,
+          Dialog.MessageBox.OKCANCEL, () => {
+            this.$store.dispatch('users/delete', {id: user.id});
+          });
+    },
+    presentUser: function (user) {
+      let content = `<div class="cl-dialog">
 <div class="tabular">
 <p><span class="label">User ID: </span><span>${user.userId}</span></p>
 <p><span class="label">Name: </span><span>${user.name}</span></p>
@@ -80,32 +80,36 @@
 <p><span class="label">Role: </span><span>${user.userRoleName()}</span></p>
 </div>
 </div>`;
-                new Dialog({
-                    content: content
-                });
-            }
-        },
-        computed: mapState({
-          users: state => state.users.users,
-         // users: state => state.Site.Users,
-            fetcher: state => state.users.fetcher
-        }),
-        mounted() {
-          this.$root.setTitle(': Users');
-          this.$store.dispatch('users/fetch');
-
-	        // Add the 'Add User' option to the the nav2 navigation bar
-	        if(this.management) {
-		        this.addComponent = this.$root.console.components.addNav2Link(this, 'Add User', 5, () => {
-			        this.$router.push(Site.root + '/cl/console/management/user/new');
-		        });
-	        }
-        },
-      beforeUnmount() {
-          this.$root.console.components.removeNav2(this, this.addComponent);
-        },
-        components: {
-            'fetcher': FetcherVue
-        }
+      new Dialog({
+        content: content
+      });
     }
+  },
+  computed: mapState({
+    users: state => state.users.users,
+    // users: state => state.Site.Users,
+    fetcher: state => state.users.fetcher
+  }),
+  mounted() {
+    this.$root.setTitle(': Users');
+    this.$store.dispatch('users/fetch');
+
+    // Add the 'Add User' option to the the nav2 navigation bar
+    if (this.management) {
+      this.addComponent = this.$root.console.components.addNav2Link(this, 'Add User', 5, () => {
+        this.$router.push(Site.root + '/cl/console/management/user/new');
+      });
+    } else {
+      this.addComponent = null
+    }
+  },
+  beforeUnmount() {
+    if(this.addComponent) {
+      this.$root.console.components.removeNav2(this, this.addComponent);
+    }
+  },
+  components: {
+    'fetcher': FetcherVue
+  }
+}
 </script>
